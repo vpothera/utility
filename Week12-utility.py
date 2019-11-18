591abfa432f7e4190ff3cc8e375447e7322f8261
varada pothera <vpothera@mymail.mines.edu>
2019-11-18 13:02:46 -0700

    loadfile function

--- Week12-utility.py
+++ Week12-utility.py
@@ -3,3 +3,11 @@ def PrintOutput(input_string):
 user_string=input('Enter a string:')
 print(f'>>>PrintOutput({user_string})')
 PrintOutput(user_string)
+
+def LoadFile (filename):
+    f=open(filename, 'r+')
+    contents=f.read()
+    contents=contents.split("\n")
+    return contents
+user_file=input("Enter the file:")
+LoadFile(user_file)
