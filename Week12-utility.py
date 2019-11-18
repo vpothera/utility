c7763f0ec28c31fa92ff6fe4500eff249f899d88
varada pothera <vpothera@mymail.mines.edu>
2019-11-18 13:10:13 -0700

    updatestring

--- Week12-utility.py
+++ Week12-utility.py
@@ -11,3 +11,14 @@ def LoadFile (filename):
     return contents
 user_file=input("Enter the file:")
 LoadFile(user_file)
+
+def UpdateString (orig_string, replace_string, index):
+    orig_string=list(orig_string)
+    orig_string[index]=replace_string
+    orig_string=''.join(orig_string)
+    print('OUTPUT', orig_string)
+user_string=str(input('Enter string:'))
+user_string2=str(input('Enter string:'))
+user_index=int(input('Enter index:'))
+print(f'>>>UpdateString({user_string}, {user_string2}, {user_index})')
+UpdateString(user_string, user_string2, user_index)
